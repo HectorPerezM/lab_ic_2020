00fbde4706f21a936ef503e959fb29fe5af5c21f
HectorPerezM <hector.perez.m@usach.cl>
2020-05-04 21:09:36 -0400

    Avance lab1

---
+++ lab_1/scripts_lab1.R
@@ -0,0 +1,21 @@
+# Laboratorio N°1
+
+# Set working directory
+# change as needed
+workdir_path <- "~/code/github.com/HectorPerezM/lab_ic_2020/lab_1"
+setwd(workdir_path)
+
+# Dataset path
+dataset_names_path <- "../dataset/mushroom/agaricus-lepiota.names"
+dataset_path <- "../dataset/mushroom/agaricus-lepiota.data"
+
+data <- read.csv(dataset_path, header = false)
+
+# Show dataset dimension
+dim(data)
+
+# Show list of components of data
+names(data)
+
+# Show first few rows
+head(data)
