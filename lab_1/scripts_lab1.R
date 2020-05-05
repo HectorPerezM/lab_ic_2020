f57575553e74f4be91554a500f0f2bc148c9f619
HectorPerezM <hector.perez.m@usach.cl>
2020-05-04 22:02:13 -0400

    Avance en manejar info de V1

--- lab_1/scripts_lab1.R
+++ lab_1/scripts_lab1.R
@@ -9,7 +9,7 @@ setwd(workdir_path)
 dataset_names_path <- "../dataset/mushroom/agaricus-lepiota.names"
 dataset_path <- "../dataset/mushroom/agaricus-lepiota.data"
 
-data <- read.csv(dataset_path, header = false)
+data <- read.csv(dataset_path, header = FALSE)
 
 # Show dataset dimension
 dim(data)
@@ -19,3 +19,24 @@ names(data)
 
 # Show first few rows
 head(data)
+
+# Display internal structure of data
+str(data)
+sapply(data, typeof)
+sapply(data, class)
+
+# Histogram of "e: edible" & "p: poisonous"
+barplot(table(data["V1"]))
+
+# Calculate Var
+#numeric_v1 <- as.numeric(as.character(data["V1"])) 
+s <- as.numeric(unlist(data["V1"]))
+# Get Var & SD of V1
+var(s)
+sd(s)
+
+# Shows variable info of column "V1"
+round(addmargins(a))
+
+# Shows % of "edible" & "poisonous"
+prop.table(table(data["V1"]))
