9f869c2c681ffcc646520511460687d68cfa3245
HectorPerezM <hector.perez.m@usach.cl>
2020-05-06 00:44:52 -0400

    ploteo de barras para la primera variable

--- lab_1/scripts_lab1.R
+++ lab_1/scripts_lab1.R
@@ -1,5 +1,11 @@
 # Laboratorio N°1
 
+# Load ggpubr library for better plots
+library(ggpubr)
+
+# Load dplyr to handle classes in columns
+library(dplyr)
+
 # Set working directory
 # change as needed
 workdir_path <- "~/code/github.com/HectorPerezM/lab_ic_2020/lab_1"
@@ -14,33 +20,47 @@ data <- read.csv(dataset_path, header = FALSE)
 # Show dataset dimension
 dim(data)
 
+# Rename columns for better comprehension
+names(data) <- c("type", "cap_shape", "cap_surface", "cap_color", "has_bruises", "odor", "gill_attachment", "gill_spacing", "gill_size", "gill_color", "stalk_shape", "stalk_root", "stalk_surface_above_ring", "stalk_surface_below_ring", "stalk_color_above_ring", "stalk_color_below_ring", "veil_type", "veil_color", "ring_number", "ring_type", "spore_print_color", "population", "habitat")
+
 # Show list of components of data
 names(data)
 
-# Show first few rows
-head(data)
+# Show first 2 rows
+head(data, n=2)
 
 # Display internal structure of data
 str(data)
 sapply(data, typeof)
 sapply(data, class)
 
-# Histogram of "e: edible" & "p: poisonous"
-barplot(width=c(5, 10), space=0, names.arg=c("Comestible", "Venenoso"), table(data["V1"]), legend="Histograma de Hongos 'Comestibles' v/s 'Venenosos'", col = "BLUE")
+typeof(data)
+class(data)
 
-# Calculate Var
-#numeric_v1 <- as.numeric(as.character(data["V1"])) 
-s <- as.numeric(unlist(data["V1"]))
-# Get Var & SD of V1
-var(s)
-sd(s)
+# N° columns dataset
+ncol(data)
+# N° rows dataset
+nrow(data)
 
-hist(s, freq=TRUE, col="RED")
+# ---------- Analysis V1 -> type -------------------------
+# type:
+#   e -> edible
+#   p -> poisonous
 
-# Shows variable info of column "V1"
-round(addmargins(a))
+# Count observations for each class
+type_table <- table(data$type)
+print(type_table[1])
 
-# Shows % of "edible" & "poisonous"
-# Multiply by 100 to adjust
-v1_percentage <- 100*prop.table(table(data["V1"]))
-cat(v1_percentage)
+# Get percentage of each class: "e" & "p"
+type_percentage <- 100*prop.table(table(data$type))
+print(type_percentage)
+
+# Discutir si calcular variancia , media , etc
+
+# dataframe formatted to create barplot
+type_df <- data.frame(type=c("edible", "poisonous"), n_mushrooms=c(type_table[1], type_table[2]))
+
+# Barplot of type of 
+ggbarplot(type_df, x = "type", y = "n_mushrooms", xlab=c("Type"), ylab="# of Mushrooms", 
+          fill="type", color="type", palette = c("#f6f578", "#06623b"), width = c(0.4, 0.4), 
+          title = "Consumable Mushrooms", label = TRUE, label.pos = "out")
